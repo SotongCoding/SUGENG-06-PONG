87d01220cdc240a84de165c16663562627ea9b4c
Sugeng Prasetiyono <37234015+SotongCoding@users.noreply.github.com>
2022-08-02 17:50:26 +0700

    UI And Game Manager
    - Adding UI Score max and Player Get
    - Change Resolution for Build

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
-
-
     [SerializeField] int leftScore;
     [SerializeField] int rightScore;
 
     [SerializeField] int maxScore;
 
+
+    [SerializeField] Text rightScore_text, leftScore_text;
+    [SerializeField] Text maxScore_text;
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        maxScore_text.text = maxScore.ToString();
     }
     public void IncreaseScore(bool forRight)
     {
@@ -25,6 +32,9 @@ public class ScoreManager : MonoBehaviour
 
         if (rightScore >= maxScore) GameOver(true);
         if (leftScore >= maxScore) GameOver(false);
+
+        rightScore_text.text = rightScore.ToString();
+        leftScore_text.text = leftScore.ToString();
 
     }
 
